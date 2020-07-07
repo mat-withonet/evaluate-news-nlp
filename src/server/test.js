5bb17c2d1aef0d0b550afeea6b3572f6a818017b
Mat <63187287+mat-withonet@users.noreply.github.com>
2020-07-07 13:15:57 +0200

    Working finally

--- src/server/test.js
+++ src/server/test.js
@@ -2,5 +2,5 @@ import { makeResponse } from './index.js'
 
 
 test('This function has a return so it should be defined', () => {
-  expect(makeResponse("blahblah", {"subjectivity": "blehblehbleh", "polarity": "blublu"})).toBeDefined();
+  expect(makeResponse("Words", {"More words": "Even more words", "polarity": "Coulpe words"})).toBeDefined();
 });
